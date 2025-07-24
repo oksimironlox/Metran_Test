f2bebcf5ffd41d5c9aefa104e35b755cb4ba4d2b
Настя <nastamezenceva9155@gmail.com>
2025-07-24 12:32:02 +0500

    Front

--- Metran_Test/Form1.Designer.cs
+++ Metran_Test/Form1.Designer.cs
@@ -1,6 +1,8 @@
-﻿namespace Metran_Test
+﻿using System;
+
+namespace Metran_Test
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +30,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.InputIDProduct = new System.Windows.Forms.TextBox();
+            this.TestsBox = new System.Windows.Forms.GroupBox();
+            this.StartTestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // InputIDProduct
+            // 
+            this.InputIDProduct.CausesValidation = false;
+            this.InputIDProduct.Location = new System.Drawing.Point(184, 113);
+            this.InputIDProduct.Name = "InputIDProduct";
+            this.InputIDProduct.Size = new System.Drawing.Size(439, 26);
+            this.InputIDProduct.TabIndex = 1;
+            this.InputIDProduct.TabStop = false;
+            this.InputIDProduct.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TestsBox
+            // 
+            this.TestsBox.Location = new System.Drawing.Point(288, 164);
+            this.TestsBox.Name = "TestsBox";
+            this.TestsBox.Size = new System.Drawing.Size(265, 221);
+            this.TestsBox.TabIndex = 2;
+            this.TestsBox.TabStop = false;
+            this.TestsBox.Text = "Выберите тест";
+            this.TestsBox.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // StartTestButton
+            // 
+            this.StartTestButton.Location = new System.Drawing.Point(672, 115);
+            this.StartTestButton.Name = "StartTestButton";
+            this.StartTestButton.Size = new System.Drawing.Size(75, 23);
+            this.StartTestButton.TabIndex = 3;
+            this.StartTestButton.Text = "Начать тестирование";
+            this.StartTestButton.UseVisualStyleBackColor = true;
+            this.StartTestButton.Click += new System.EventHandler(this.StartTestButton_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.StartTestButton);
+            this.Controls.Add(this.TestsBox);
+            this.Controls.Add(this.InputIDProduct);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form";
+            this.Text = "Metran_Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+        
+
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox InputIDProduct;
+        private System.Windows.Forms.GroupBox TestsBox;
+        private System.Windows.Forms.Button StartTestButton;
     }
 }
 
--- Metran_Test/Form1.cs
+++ Metran_Test/Form1.cs
@@ -1,20 +1,165 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Metran_Test
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        private const int EM_SETCUEBANNER = 0x1501;
+        private RadioButton radioButton1, radioButton2, radioButton3;
+        public Form()
         {
             InitializeComponent();
+            this.Focus();
+            this.Load += Form_Load;
+            this.Resize += (s, e) =>
+            {
+                PositionTextBox();
+                PositionTestsBox();
+                ArrangeRadioButtons();
+            };
         }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            PositionTextBox();
+            SetPlaceholder(InputIDProduct, "Введите идентификатор изделия...");
+
+            CreateRadioButtons();
+            PositionTestsBox();
+            ArrangeRadioButtons();
+
+            StartTestButton.TextChanged += (s, ev) => PositionStartTestButton();
+        }
+
+        private void CreateRadioButtons()
+        {
+            radioButton1 = new RadioButton
+            {
+                Text = "Тест 1",
+                AutoSize = true
+            };
+
+            radioButton2 = new RadioButton
+            {
+                Text = "Тест 2",
+                AutoSize = true
+            };
+
+            radioButton3 = new RadioButton
+            {
+                Text = "Тест 3",
+                AutoSize = true
+            };
+
+            TestsBox.Controls.Add(radioButton1);
+            TestsBox.Controls.Add(radioButton2);
+            TestsBox.Controls.Add(radioButton3);
+        }
+
+        private void ArrangeRadioButtons()
+        {
+            if (TestsBox.Controls.Count == 0) return;
+
+            int padding = 20;
+            int spacing = 10;
+            int totalHeight = padding;
+
+            foreach (Control control in TestsBox.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    totalHeight += control.Height + spacing;
+                }
+            }
+
+            totalHeight = totalHeight - spacing + padding;
+
+            TestsBox.Height = totalHeight;
+
+            int startY = padding;
+            int controlWidth = TestsBox.ClientSize.Width - 2 * padding;
+
+            foreach (Control control in TestsBox.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    control.Location = new Point(padding, startY);
+                    control.Width = controlWidth;
+                    startY += control.Height + spacing;
+                }
+            }
+
+            PositionTestsBox();
+        }
+
+        private void SetPlaceholder(TextBox textBox, string placeholder)
+        {
+            if (textBox.IsHandleCreated)
+            {
+                SendMessage(textBox.Handle, EM_SETCUEBANNER, 0, placeholder);
+            }
+            else
+            {
+                textBox.HandleCreated += (s, e) =>
+                {
+                    SendMessage(textBox.Handle, EM_SETCUEBANNER, 0, placeholder);
+                };
+            }
+        }
+
+
+        private void PositionTextBox()
+        {
+            InputIDProduct.Left = (this.ClientSize.Width - InputIDProduct.Width - StartTestButton.Width) / 2;
+            InputIDProduct.Top = (this.ClientSize.Height - statusStrip1.Height) / 4;
+
+            PositionStartTestButton();
+        }
+
+        private void PositionTestsBox()
+        {
+            TestsBox.Left = (this.ClientSize.Width - TestsBox.Width) / 2;
+            int inputBottom = InputIDProduct.Top + InputIDProduct.Height;
+            int statusTop = this.ClientSize.Height - statusStrip1.Height;
+            int availableSpace = statusTop - inputBottom;
+            TestsBox.Top = inputBottom + (availableSpace - TestsBox.Height) / 2;
+        }
+
+        private void PositionStartTestButton()
+        {
+            using (Graphics g = StartTestButton.CreateGraphics())
+            {
+                SizeF textSize = g.MeasureString(StartTestButton.Text, StartTestButton.Font);
+                int buttonWidth = (int)textSize.Width + 20;
+                StartTestButton.Width = Math.Min(buttonWidth, 200);
+
+                StartTestButton.Height = InputIDProduct.Height * 3;
+            }
+
+            StartTestButton.Left = InputIDProduct.Right + 10;
+            StartTestButton.Top = InputIDProduct.Top - InputIDProduct.Height;
+        }
+
+
+        private void StartTestButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
